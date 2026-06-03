4f810a224179f8d9c46310990dacf8f8df23a715
peter-mwau <kyalepeter2000@gmail.com>
2026-06-03 13:06:49 +0300

    Got rid of lint errors

--- src/contexts/APSDEXContext.jsx
+++ src/contexts/APSDEXContext.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { useContext, createContext, useState } from "react";
 import PropTypes from "prop-types";
 import { ADDRESSES } from "../constants/addresses";
