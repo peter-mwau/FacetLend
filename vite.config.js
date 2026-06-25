ab6ac2b0994b436601c5b3630b1150c0617c09f5
peter-mwau <kyalepeter2000@gmail.com>
2026-06-25 11:51:57 +0300

    Updated the vite config & added  an icons folder

--- vite.config.js
+++ vite.config.js
@@ -25,29 +25,65 @@ export default defineConfig({
       maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
     },
     manifest: {
-      name: 'FacetLend',
-      short_name: 'FacetLend',
-      build: {
-        chunkSizeWarningLimit: 3000,
-      },
-      description: 'Unlock every facet of your borrowing power. ',
-      theme_color: '#1a202c',
+      name: "Facetlend",
+      short_name: "FL",
+      description: "Unlock every facet of your borrowing power.",
+      start_url: "/",
+      scope: "/",
+      display: "standalone",
+      orientation: "portrait",
+      background_color: "#ffffff",
+      theme_color: "#000000",
       icons: [
         {
-          src: 'facetlend_logo.png',
-          sizes: '192x192',
-          type: 'image/png'
+          src: "icons/icon-48x48.png",
+          sizes: "48x48",
+          type: "image/png"
         },
         {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
+          src: "icons/icon-72x72.png",
+          sizes: "72x72",
+          type: "image/png"
         },
         {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable'
+          src: "icons/icon-96x96.png",
+          sizes: "96x96",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-128x128.png",
+          sizes: "128x128",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-144x144.png",
+          sizes: "144x144",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-152x152.png",
+          sizes: "152x152",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-256x256.png",
+          sizes: "256x256",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-384x384.png",
+          sizes: "384x384",
+          type: "image/png"
+        },
+        {
+          src: "icons/icon-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
         }
       ]
     }
